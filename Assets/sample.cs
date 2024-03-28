4d4a744300a6b1c3657123419724fac933658fc5
BonyaSlayer <zlobasterus@gmail.com>
2024-03-28 11:56:34 +0500

    ref

---
+++ Assets/sample.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sample : MonoBehaviour
+{
+
+    private int _num;
+    bool Get;
+    void Start()
+    {
+        Debug.Log(Get);
+    }
+    
+}
