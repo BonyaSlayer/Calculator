7f7aa283fe84cd4f35e8890f3998b1924f38e594
BonyaSlayer <zlobasterus@gmail.com>
2024-03-29 13:02:14 +0500

    Ref Script logic

--- Assets/MyAssets/CalcScript.cs
+++ Assets/MyAssets/CalcScript.cs
@@ -13,22 +13,75 @@ public class CalcScript : MonoBehaviour
     private int _a;
     private int _b;
 
-    
+
 
     public void pressSum()
     {
-        _a = int.Parse(a.text);
-        _b = int.Parse(b.text);
-        int c = _a + _b;
-        answer.text = c.ToString();
+        try
+        {
+            _a = int.Parse(a.text);
+            _b = int.Parse(b.text);
+            int c = _a + _b;
+            answer.text = c.ToString();
+        }
+        catch(Exception e)
+        {
+            answer.text = "Попробуй еще раз!";
+        }
     }
 
     public void pressSub()
     {
-        _a = int.Parse(a.text);
-        _b = int.Parse(b.text);
-        int c = _a - _b;
-        answer.text = c.ToString();
+        try
+        {
+            _a = int.Parse(a.text);
+            _b = int.Parse(b.text);
+            int c = _a - _b;
+            answer.text = c.ToString();
+        }
+        catch(Exception e)
+        {
+            answer.text = "Попробуй еще раз!";
+        }
+    }
+
+    public void pressMult()
+    {
+        try
+        {
+            _a = int.Parse(a.text);
+            _b = int.Parse(b.text);
+            int c = _a * _b;
+            answer.text = c.ToString();
+        }
+        catch (Exception e) 
+        {
+            answer.text = "Попробуй еще раз!";
+        }          
+    }
+
+
+    public void pressDiv()
+    {        
+        try
+        {
+            _a = int.Parse(a.text);
+            _b = int.Parse(b.text);
+            int c = _a / _b;
+            answer.text = c.ToString();
+        }
+        catch (Exception e)
+        {
+            if (e is DivideByZeroException) 
+            {
+                answer.text = "На ноль делить нельзя!";
+            }
+            else
+            {
+                answer.text = "Попробуй еще раз!";
+            }
+            
+        }        
     }
 
 }
